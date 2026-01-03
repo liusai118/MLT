a15e5e77bdd71c1c655b1ff0307a20ff9735bbc2
liusai118 <1543916185@qq.com>
2026-01-03 19:48:35 +0800

    Update RNAseq.sh
    Add annotation

--- Result5/RNAseq.sh
+++ Result5/RNAseq.sh
@@ -1,26 +1,38 @@
 input=/home/liusai/RNA-seq/data/MLT
 output=/home/liusai/RNA-seq/result/MLT
-mkdir -p $output/fastp
-mkdir -p $output/sam
-mkdir -p $output/bam
-mkdir -p $output/FC
+
+mkdir -p $output/{fastp,bam,FC}
 
 cat MLT.txt | while read id
 do
+    echo "Processing $id"
+
+    ## ---------- fastp ----------
     mkdir -p $output/fastp/${id}
-    fastp -i $input/${id}.R1.raw.fastq.gz -o $output/fastp/${id}_filter_R1.fq.gz  \
-          -I $input/${id}.R2.raw.fastq.gz -O $output/fastp/${id}_filter_R2.fq.gz \
-          -h $output/fastp/${id}/${id}_report.html
+    fastp -w 8 \
+      -i $input/${id}.R1.raw.fastq.gz \
+      -I $input/${id}.R2.raw.fastq.gz \
+      -o $output/fastp/${id}_R1.clean.fq.gz \
+      -O $output/fastp/${id}_R2.clean.fq.gz \
+      -h $output/fastp/${id}/${id}.html \
+      -j $output/fastp/${id}/${id}.json
 
-    mkdir -p $output/sam/${id}
-    hisat2 -t -p 32 -x /home/liusai/RNA-sequence/index/hsa \
-           -1 $output/fastp/${id}_filter_R1.fq.gz \
-           -2 $output/fastp/${id}_filter_R2.fq.gz \
-           -S $output/sam/${id}/${id}.sam
+    ## ---------- HISAT2 + 排序 ----------
+    hisat2 --dta -p 32 \
+      -x /home/liusai/RNA-sequence/index/hsa \
+      -1 $output/fastp/${id}_R1.clean.fq.gz \
+      -2 $output/fastp/${id}_R2.clean.fq.gz \
+    | samtools sort -@10 -o $output/bam/${id}_hisat2.sorted.bam
 
-    samtools view -@10 -bS $output/sam/${id}/${id}.sam > $output/FC/${id}.bam
-
+    samtools index $output/bam/${id}_hisat2.sorted.bam
 done
-featureCounts -T 10 -a /home/liusai/RNA-sequence/index/GCF_000001405.40_GRCh38.p14_genomic.gtf -o read.count -p -t exon -g gene_id  *.bam
 
+## ---------- featureCounts ----------
+cd $output/FC
 
+featureCounts -T 10 \
+-a /home/liusai/RNA-sequence/index/GCF_000001405.40_GRCh38.p14_genomic.gtf \
+-o read.count \
+-p -B -C \
+-t exon -g gene_id \
+*_hisat2.sorted.bam
